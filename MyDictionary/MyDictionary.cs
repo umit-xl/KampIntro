227fad161c503c8e68ed8e5a9c49c1f3e59952ec
Ümit ARLI <wtfumit@outlook.com.tr>
2023-03-05 19:00:49 +0300

    4.gün 4.ödev

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<Tkey, Tvalue>
+    {
+        Tkey[] keys;
+        Tvalue[] values;
+
+        public MyDictionary()
+        {
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+        }
+
+
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            Tkey[] tKeys = keys;
+            Tvalue[] tValues = values;
+
+            keys = new Tkey[keys.Length+1];
+            values = new Tvalue[values.Length+1];
+
+            for (int i = 0; i < tKeys.Length; i++)
+            {
+                keys[i] = tKeys[i];
+            }
+
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tValues.Length; i++)
+            {
+                values[i] = tValues[i];
+            }
+            values[values.Length - 1] = value;
+
+
+            
+
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using MyDictionary;
+using System;
+using System.Collections.Generic;
+
+// Bir Dictionary örneği oluşturma.
+Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+// Dictionary'ye eleman ekleme.
+dictionary.Add("elma", 50);
+dictionary.Add("armut", 100);
+dictionary.Add("portakal", 75);
+
+// Dictionary'deki tüm elemanların yazdırılması.
+foreach (KeyValuePair<string, int> kvp in dictionary)
+{
+    Console.WriteLine("Anahtar = {0}, Değer = {1}", kvp.Key, kvp.Value);
+}
+
+// Dictionary'deki belirli bir elemanın değerini güncelleme.
+dictionary["elma"] = 30;
+
+// Dictionary'deki belirli bir elemanın değerini yazdırma.
+int value;
+if (dictionary.TryGetValue("elma", out value))
+{
+    Console.WriteLine("elma değeri: {0}", value);
+}
+else
+{
+    Console.WriteLine("elma anahtarı yok.");
+}
+
+
+
+
+//Kendi Dictionary yapımızı oluşturalım. 
+
+MyDictionary<int, string> musteriBilgileri = new MyDictionary<int, string>();
+
+musteriBilgileri.Add(13, "Ümit");
+musteriBilgileri.Add(25, "Alba");
+
+
+
+
+
